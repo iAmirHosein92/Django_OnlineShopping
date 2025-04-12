f87af24f0c94c87e7fb610c0c5778f2b212fd872
iAmirHosein92 <iamirhoseinbdi@gmail.com>
2025-04-12 11:18:54 +0330

    S0 , 6

---
+++ onlineshopping/accounts/forms.py
@@ -0,0 +1,42 @@
+from django import forms
+from .models import User
+from django.core.exceptions import ValidationError
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+
+
+class UserCreationForm(forms.ModelForm):
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('email', 'phone_number', 'full_name')
+
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password1'] and cd['password2'] and cd['password1'] != cd['password2']:
+            raise ValidationError('Passwords don\'t match')
+        return cd['password2']
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password1'])
+        if commit:
+            user.save()
+        return user
+
+
+class UserChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField(help_text='You can change password by using <a href=" ../password/">this form</a>.')
+
+    class Meta:
+        model = User
+        fields = ('email', 'phone_number', 'full_name', 'password', 'last_login')
+
+
+class UserRegistrationForm(forms.Form):
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'}))
+    full_name = forms.CharField(label= 'Full Name')
+    phone = forms.CharField(max_length=11)
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
--- onlineshopping/accounts/managers.py
+++ onlineshopping/accounts/managers.py
@@ -2,23 +2,23 @@ from django.contrib.auth.models import BaseUserManager
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, phone_number, email, fullname, password):
+    def create_user(self, phone_number, email, full_name, password):
         if not phone_number:
             raise ValueError('The phone number must be set')
 
         if not email:
             raise ValueError('The email must be set')
 
-        if not fullname:
+        if not full_name:
             raise ValueError('The fullname must be set')
 
-        user = self.model(phone_number=phone_number, email=self.normalize_email(email), full_name=fullname)
+        user = self.model(phone_number=phone_number, email=self.normalize_email(email), full_name=full_name)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, phone_number, email, fullname, password):
-        user = self.create_user(phone_number, email, fullname, password)
+    def create_superuser(self, phone_number, email, full_name, password):
+        user = self.create_user(phone_number, email, full_name, password)
         user.is_admin = True
         user.save(using=self._db)
         return user
